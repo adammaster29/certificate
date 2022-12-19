238e7fbb7b6aeefa60a6affbf44d10e0710fa830
adammaster29 <104658788+adammaster29@users.noreply.github.com>
2022-12-19 12:52:02 -0500

    update image

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-
 const Home = ({certificates}) => {
     return (
         <div className="App">
         <div className='container--hello'>
            <h2 className="title"> <span >  Hello!,</span> welcome to my certificate</h2>
-           <img className='img-adam' src="/src/img/ADAM.jpg" alt="adam" />
+           <img className='img-adam' src='image/ADAm.jpg' alt="adam" />
         <br/><br /><p>Name: <span>Adalberto agudelo</span></p>
         <p>Age:<span> &nbsp;   29 year old</span></p>
         </div>
